791217d6f8a745d9c6f3c1d0774583104ac7e6e2
sss <sujianfu199@qq.com>
2019-06-17 15:55:15 +0800

    Initial Commit

---
+++ SLog/SLog.h
@@ -0,0 +1,19 @@
+//
+//  SLog.h
+//  SLog
+//
+//  Created by SSS on 2019/6/17.
+//  Copyright © 2019 SSS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SLog.
+FOUNDATION_EXPORT double SLogVersionNumber;
+
+//! Project version string for SLog.
+FOUNDATION_EXPORT const unsigned char SLogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SLog/PublicHeader.h>
+
+
